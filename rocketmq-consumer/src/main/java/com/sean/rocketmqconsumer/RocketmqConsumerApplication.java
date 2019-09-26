e260d68f69b9d01044d35203c2720aa1e40f0150
殷海翔 <yin.hx@burgeon.cn>
2019-09-26 11:16:20 +0800

    init

---
+++ rocketmq-consumer/src/main/java/com/sean/rocketmqconsumer/RocketmqConsumerApplication.java
@@ -0,0 +1,16 @@
+package com.sean.rocketmqconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.stream.annotation.EnableBinding;
+import org.springframework.cloud.stream.messaging.Sink;
+
+@SpringBootApplication
+@EnableBinding({Sink.class})
+public class RocketmqConsumerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RocketmqConsumerApplication.class, args);
+    }
+
+}
